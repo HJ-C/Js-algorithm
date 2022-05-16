9f8390fbbe3542669d0775d99a1169ab9c7c67e9
HJ-C <swch56@naver.com>
2022-05-16 17:53:26 +0900

    블루투포스

---
+++ 4-1.js
@@ -0,0 +1,51 @@
+(1)
+function solution(n, arr){
+    let answer;
+    let max = Number.MIN_SAFE_INTEGER;
+
+    for(let x of arr){
+       let sum=0;
+       let tmp=x;
+
+       while(tmp){
+          sum+=(tmp%10);
+          tmp=Math.floor(tmp/10)
+       }
+       if(sum>max){
+           max=sum;
+           answer=x
+       }else if( sum === max){
+        if(x>answer) answer =x;
+    }
+    }
+
+    return answer;
+}
+
+let arr=[128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
+
+(2)
+function solution(n, arr){
+    let answer;
+    let max = Number.MIN_SAFE_INTEGER;
+
+    for(let x of arr){
+
+        let sum=x.toString().split('').reduce((a,b)=>{
+            a+Number(b);
+        },0);
+
+       if(sum>max){
+           max=sum;
+           answer=x
+       }else if( sum === max){
+        if(x>answer) answer =x;
+    }
+    }
+
+    return answer;
+}
+
+let arr=[128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
