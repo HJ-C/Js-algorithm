513eafb34456fb79a9e03c211905ab7fa4f35931
HJ-C <swch56@naver.com>
2022-05-25 19:36:52 +0900

    삽입정렬

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-function solution(arr){
-    let answer=arr;
+function solution(size, arr){
     
-    for(let i=0; i<arr.length; i++){
-        let tmp = arr[i],j;
-        for(j=i-1; j>=0; j--){
-            if(arr[j]>tmp){
-                arr[j+1] = arr[j]
-            }else{
-                break;
-            }
-        }
-        arr[j+1] = tmp
-    }
+
     return answer;
 }
 
-let arr=[11, 7, 5, 6, 10, 9];
-console.log(solution(arr));
+let arr=[1, 2, 3, 2, 6, 2, 3, 5, 7];
+console.log(solution(5, arr));
